11d77b7a59a1011a59bc758b5e2df81e1fbef2bd
eelnevs <mwl82701@gmail.com>
2024-04-14 23:33:03 -0500

    improve functionality, add search results

--- js/travel_recommendation.js
+++ js/travel_recommendation.js
@@ -1,14 +1,16 @@
-function fetchHTML(file) {
-  fetch(file)
-    .then((Response) => {
-      return Response.text();
-    })
-    .then((html) => {
-      document.querySelector("#main-content").innerHTML = html;
-    })
-    .catch((error) => {
-      console.error("Error loading content:", error);
-    });
+async function fetchHTML(file) {
+  try {
+    const response = await fetch(file);
+    const html = await response.text();
+    return html;
+  } catch (error) {
+    window.alert(`Error getting page from ${file}`);
+    console.error(error);
+  }
+}
+
+async function loadMainContent(file) {
+  document.querySelector("#main-content").innerHTML = await fetchHTML(file);
 }
 
 function searchBar(enable) {
@@ -16,18 +18,122 @@ function searchBar(enable) {
 }
 
 document.querySelector("#homeBtn").addEventListener("click", () => {
-  fetchHTML("home.html");
+  loadMainContent("home.html");
   searchBar("visible");
 });
 document.querySelector("#aboutUsBtn").addEventListener("click", () => {
-  fetchHTML("about_us.html");
+  loadMainContent("about_us.html");
   searchBar("hidden");
 });
 document.querySelector("#contactUsBtn").addEventListener("click", () => {
-  fetchHTML("contact_us.html");
+  loadMainContent("contact_us.html");
   searchBar("hidden");
 });
 
 function bookButtonClick() {
   window.open("https://www.expedia.com/", "_blank").focus();
 }
+
+async function fetchRecommendations() {
+  try {
+    const response = await fetch("../data/travel_recommendation_api.json");
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Cannot fetch data:", error);
+  }
+  return undefined;
+}
+
+function setDestination(des) {
+  const lastCard = document.querySelector("#recommendations").lastChild;
+  lastCard.querySelector(".destination-name").textContent = des.name;
+  if (des.notFound == 1) {
+    lastCard.querySelector(".destination-img").src = "";
+    lastCard.querySelector(".destination-visitBtn").style.display = "none";
+  } else {
+    lastCard.querySelector(".destination-img").src = des.imageUrl;
+    lastCard.querySelector(".destination-description").textContent =
+      des.description;
+    lastCard.querySelector(".destination-visitBtn").style.display = "flex";
+  }
+}
+
+async function showRecommendations(destinations) {
+  const recommendationsDiv = document.querySelector("#recommendations");
+  const bar = document.createElement("div");
+  bar.classList.add("result-bar");
+  recommendationsDiv.appendChild(bar);
+  if (destinations == undefined) {
+    destinations = [
+      {
+        name: "No destination found",
+        notFound: 1,
+      },
+    ];
+  }
+  destinations.forEach((des) => {
+    fetchHTML("search_result.html")
+      .then((html) => {
+        const newCard = document.createElement("div");
+        recommendationsDiv.appendChild(newCard);
+        const lastCard = document.querySelector("#recommendations").lastChild;
+        lastCard.outerHTML = html;
+      })
+      .then(() => {
+        setDestination(des);
+      })
+      .catch((error) => console.error("fail to add destination card:", error));
+  });
+}
+
+async function search() {
+  clearSearch(false);
+  const word = document.querySelector("#searchBar input").value.toLowerCase();
+  if (word.length > 0) {
+    const destinations = await fetchRecommendations();
+    var find = Object.keys(destinations).find((x) =>
+      x.includes(word.substr(0, word.length - 1))
+    );
+    if (!find) {
+      find = destinations.countries.find((x) =>
+        x.name.toLowerCase().includes(word.substr(0, word.length - 1))
+      );
+      if (find) showRecommendations(find.cities);
+      else showRecommendations(undefined);
+    } else {
+      if (find == "countries")
+        showRecommendations(
+          destinations.countries.flatMap((x) => x.cities),
+          true
+        );
+      else showRecommendations(destinations[find]);
+    }
+  }
+}
+
+function clearSearch(resetKeyWord = true) {
+  document.querySelector("#recommendations").innerHTML = "";
+  if (resetKeyWord) document.querySelector("#searchBar input").value = "";
+}
+
+function getTime(timeZone) {
+  const options = {
+    timeZone: timeZone,
+    hour12: true,
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  };
+  const localeTime = new Date().toLocaleTimeString("en-US", options);
+  console.log(`Current time in ${timeZone}:`, localeTime);
+  return `Current time: ${localeTime}`;
+}
+
+document.querySelector("#search").addEventListener("click", () => search());
+document.querySelector("#searchBar input").addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    search();
+  }
+});
+document.querySelector("#clear").addEventListener("click", () => clearSearch());
